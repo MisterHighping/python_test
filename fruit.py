f421198cc326b8f7214c89de0849f79ed50cf390
Gerrit Asch <gasch@Gerrit-MacBook.local>
2020-01-15 13:57:08 +0100

    Python Script

---
+++ fruit.py
@@ -0,0 +1,9 @@
+import random
+
+picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit == 'orange':
+	print("I have a orange", picked_fruit)
+elif picked_fruit == 'strawberry':
+	print("I have a red", picked_fruit)
+else:
+	print("I have a yellow", picked_fruit)
